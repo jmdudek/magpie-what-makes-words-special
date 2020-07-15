c9623e631d8ca0a62ab9f1a484c37f7e21f5331c
jmdudek <64210838+jmdudek@users.noreply.github.com>
2020-07-15 13:26:01 +0200

    Update 06_main.js

--- 06_main.js
+++ 06_main.js
@@ -26,7 +26,7 @@ $("document").ready(function() {
         ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "156",
+            experimentID: "",
             serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
